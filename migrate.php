84396f03710cb6a20de23dcf314b8921798ec4d8
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2020-06-08 20:23:43 +0600

    fix some edge cases and move migration runner totally in Migrate class

--- migrate.php
+++ migrate.php
@@ -15,12 +15,12 @@ use Norm\Migrate;
 // Do not add abstract models or models that you only use for inheritance in this
 // list as they are passive models.
 $migrate_native = [
-    models\Users::class,
-    models\UserProfile::class,
+    models\Users::class,        // example
+    models\UserProfile::class,  // example
 ];
 
 
-// native[1]: models that do not yet have a db table or whose db table was created by this migrate script.
+// native[1]: models that do not yet have a db table or there db table was created by this migrate script.
 // If your model already have a db table that was created elsewhere and is in sync with current model
 // definition, you should add it to the below $migrate_alien list.
 
@@ -32,76 +32,10 @@ $migrate_alien = [
 ];
 
 
-
-
-
-
-
-
 ################################################################################
-########################## Migration runners ###################################
+########################## Migration runner ####################################
 ################################################################################
 
-define('APPLY_METHOD_ALL', 'all');
-define('APPLY_METHOD_NONE', 'none');
-define('APPLY_METHOD_DEFAULT', '');
-
-if(!empty($argv[1]) && $argv[1] == 'apply'){
-    $apply_method = APPLY_METHOD_ALL;
-    $apply = true;
-}
-elseif(!empty($argv[1]) && $argv[1] == 'dry'){
-    $apply_method = APPLY_METHOD_NONE;
-    $apply = false;
-}
-else{
-    $apply_method = APPLY_METHOD_DEFAULT;
-    $apply = false;
-}
-
-
-
-function get_ans(&$apply_method, &$apply, $status){
-    $msg = "
-Type a command to specify what you want to do:
-    yes     I want to apply this migration
-    none    I do not want to apply any migration.
-    all     I want to apply all migrations (Dangerous action).
-
-Just hit enter to not apply migration for this model.
-
-";
-    if(empty($apply_method) && $status){
-        echo $msg;
-        $ans = readline("Type a command: ");
-        readline_add_history($ans);
-        if($ans == 'yes' || $ans == 'all'){
-            if($ans == 'all'){
-                $apply_method = APPLY_METHOD_ALL;
-                $apply = true;
-            }
-            return TRUE;
-        }elseif($ans == 'none'){
-            $apply_method = APPLY_METHOD_NONE;
-        }
-    }
-    return FALSE;
-}
-
-// The native migration runner
-foreach($migrate_native as $class){
-    $status = Migrate::run($class, $apply);
-    if(get_ans($apply_method, $apply, $status)){
-        Migrate::run($class, true);
-    }
-}
-
-// The alien migration runner
-foreach($migrate_alien as $class){
-    $status = Migrate::run_alien($class, $apply);
-    if(get_ans($apply_method, $apply, $status)){
-        Migrate::run_alien($class, true);
-    }
-}
+Migrate::run_all($migrate_native, $migrate_alien);
 
 ################################################################################
--- src/Migrate.php
+++ src/Migrate.php
@@ -1,6 +1,46 @@
 <?php namespace Norm;
 
 class Migrate{
+    private $apply_method;
+    private $apply = false;
+    public static $apply_methods = array(
+        'apply'=> 'apply', // the value is used for CLI argument
+        'dry'=> 'dry',
+        'na'=> 'na',
+    );
+
+    public static function print_help(){
+        echo "
+Available options:
+    ".self::$apply_methods['apply']."     Apply all
+    ".self::$apply_methods['dry']."       Apply none
+    ".self::$apply_methods['na']."        N/A
+        \n";
+    }
+
+    public function __construct(){
+        global $argv;
+        if(!empty($argv[1]) && $argv[1] == self::$apply_methods['apply']){
+            $this->apply_method = self::$apply_methods['apply'];
+            $this->apply = true;
+        }
+        elseif(!empty($argv[1]) && $argv[1] == self::$apply_methods['dry']){
+            $this->apply_method = self::$apply_methods['dry'];
+            $this->apply = false;
+        }
+        elseif(!empty($argv[1]) && $argv[1] == self::$apply_methods['na']){
+            $this->apply_method = self::$apply_methods['na'];
+            $this->apply = false;
+        }
+        elseif(!empty($argv[1])){
+            self::print_help();
+            exit(1);
+        }
+        else{
+            $this->apply_method = self::$apply_methods['na'];
+            $this->apply = false;
+        }
+    }
 
     public static function run($class, $apply=false){
         return $class::_change_or_create_($apply);
@@ -17,4 +57,52 @@ class Migrate{
         return $class::_change_($apply);
     }
 
+    public function get_ans($status){
+        $msg = "
+Type a command to specify what you want to do:
+    yes     I want to apply this migration
+    none    I do not want to apply any migration.
+    all     I want to apply all migrations (Dangerous/Blind action).
+
+Just hit enter to not apply migration for this model.
+        \n";
+        if($this->apply_method === self::$apply_methods['na'] && $status){
+            echo $msg;
+            $ans = readline("Type a command: ");
+            readline_add_history($ans);
+            if($ans == 'yes' || $ans == 'all'){
+                if($ans == 'all'){
+                    $this->apply_method = self::$apply_methods['apply'];
+                    $this->apply = true;
+                }
+                return TRUE;
+            }elseif($ans == 'none'){
+                $this->apply_method = self::$apply_methods['dry'];
+            }else{
+                $this->apply_method = self::$apply_methods['na'];
+            }
+        }
+        return FALSE;
+    }
+
+    public static function run_all($migrate_native, $migrate_alien){
+
+        $me = new self();
+
+        // The native migration runner
+        foreach($migrate_native as $class){
+            $status = self::run($class, $me->apply);
+            if($me->get_ans($status)){
+                self::run($class, true);
+            }
+        }
+
+        // The alien migration runner
+        foreach($migrate_alien as $class){
+            $status = self::run_alien($class, $apply);
+            if($me->get_ans($status)){
+                self::run_alien($class, true);
+            }
+        }
+    }
 }
