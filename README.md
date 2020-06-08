fe5137e529d2e42246847a57f892ce3a4793dee2
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2020-06-09 02:26:57 +0600

    add travis

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/neurobin/norm.svg?branch=release)](https://travis-ci.org/neurobin/norm)
--- tests/test.php
+++ tests/test.php
@@ -12,28 +12,28 @@ use Norm\_DB_;
 
 // var_dump($d);
 
-// $user = new User(array(
-//     'username'=> 'Jahidul Hamid',
-//     'password'=> 'fjdsfljadf;sdf',
-//     'email'=> 'fdslfjds@fdsl.com',
-// ));
+$user = new User(array(
+    'username'=> 'Jahidul Hamid',
+    'password'=> 'fjdsfljadf;sdf',
+    'email'=> 'fdslfjds@fdsl.com',
+));
 
-// // var_dump($user->_assoc());
+var_dump($user->_assoc());
 
-// $user->_save();
+$user->_save();
 
-// var_dump(User::_first());
+var_dump(User::_first());
 
 $user14 = User::_get(14);
 
 var_dump($user14);
 
-// $stmt = User::_select();
+$stmt = User::_select();
 
-// while($user=$stmt->fetch()){
-//     var_dump($user);
-//     // $user->username = 'john_doe_'.random_int(1, 34);
-//     // $user->_save();
-//     // var_dump($user);
-//     break;
-// }
+while($user=$stmt->fetch()){
+    var_dump($user);
+    $user->username = 'john_doe_'.random_int(1, 34);
+    $user->_save();
+    var_dump($user);
+    break;
+}
